27bd41036eb8b06790689841051cdae1216bf7b5
Edoardo Putti <edoardo.putti@gmail.com>
2023-02-19 10:29:14 +0100

    add creation of new article
    - validate form parameters
    - save valid articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,4 +11,17 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @article = Article.new(title: "...", body: "...")
+    if @article.save
+      redirect to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permt(:title, :body)
+  end
 end
