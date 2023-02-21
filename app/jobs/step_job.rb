b1988b98ab6c244ca1d5fec58591d7cd298a66ed
Edoardo Putti <edoardo.putti@gmail.com>
2023-02-21 17:36:38 +0100

    add actual simulation code to

--- app/jobs/step_job.rb
+++ app/jobs/step_job.rb
@@ -1,10 +1,16 @@
+require 'stringio'
+require 'simulation'
+
 class StepJob < ApplicationJob
   queue_as :default
 
   def perform(game)
-    # Do something later
     current_generation = game.generations.order(step: :desc).first
-    #TODO(edoput) actually compute the next board
-    game.generations.create(step: current_generation.step+1, board: current_generation.board)
+
+    StringIO.open(current_generation.board) do |i|
+      current_board = Matrix[*i.readlines.map{|l| l.rstrip.chars}]
+      next_board = Simulation.step(current_board)
+      game.generations.create(step: current_generation.step+1, board: Simulation.board_string(next_board))
+    end
   end
 end
---
+++ lib/simulation.rb
@@ -0,0 +1,80 @@
+require 'matrix'
+
+module Simulation
+  def read_generation(i)
+    _, gen, *_ = i.readline.split
+    Integer(gen[0...-1], 10)
+  end
+
+  def read_dimensions(i)
+    width, height, *_ = i.readline.split
+    return Integer(width, 10), Integer(height, 10)
+  end
+
+  def read_board(i, w,  h)
+    i.read((w+1) * h - 1) # read h lines, each w chars wide + line feed
+  end
+  
+  def self.neighbour_idx(i, max)
+    Range.new([i-1, 0].max, [i+1, max-1].min).to_a
+  end
+
+  DEAD = '.'
+  ALIVE = '*'
+
+  def self.from_alive(n)
+    case n
+        in 0..1
+        DEAD
+        in 2..3
+        ALIVE
+    else
+      DEAD
+    end
+  end
+
+  def self.from_dead(n)
+    case n
+    in 3
+      ALIVE
+    else
+      DEAD
+    end
+  end
+
+  def self.next_cell(current, n)
+    case current
+        in DEAD
+        from_dead(n)
+        in ALIVE
+        from_alive(n)
+    end
+  end
+
+  def self.step(board)
+    Matrix.build(board.row_count, board.column_count) do |i, j|
+      alive_count = 0
+      current_cell = board[i, j]
+      for ni in self.neighbour_idx(i, board.row_count) do
+        for nj in self.neighbour_idx(j, board.column_count) do
+          if ni != i or nj != j then
+            alive_count += board[ni,nj] == ALIVE ? 1 : 0
+          end
+        end
+      end
+      self.next_cell(current_cell, alive_count)
+    end
+  end
+
+  def self.board_string(board)
+    io = StringIO.new
+    for i in 0..board.row_count-1 do
+      for j in 0..board.column_count-1 do
+        io << board[i, j]
+      end
+      io << "\n"
+    end
+    io.string
+  end
+
+end
