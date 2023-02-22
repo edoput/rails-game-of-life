264325c47b269f04db827912fcd9811dc15a9625
Edoardo Putti <edoardo.putti@gmail.com>
2023-02-22 12:01:28 +0100

    add database seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'simulation'
+
+Game.destroy_all
+
+[
+  'beacon',
+  'bee-hive',
+  'blinker',
+  'block',
+  'boat',
+  'loaf',
+  'toad',
+  'tub',
+  'penta-decathlon',
+  'pulsar',
+].each do |name|
+  File.open("db/examples/#{name}") do |f|
+    g = Game.create!(name: name)
+    initial_step = Simulation.read_generation(f)
+    height, width = Simulation.read_dimensions(f)
+    g.width = width
+    g.height = height
+    g.save
+
+    s = Simulation.read_board(f, width, height)
+    g.generations.create(initial: true, step: initial_step, board: s)
+  end
+end
+
